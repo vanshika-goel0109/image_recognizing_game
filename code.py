e1793052cc116902aa1e90071f554cd4f10f7deb
Vanshika Goel <65799147+vanshika-goel0109@users.noreply.github.com>
2020-09-30 22:16:43 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
-
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import *
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import *
+from kivy.uix.image import AsyncImage
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
